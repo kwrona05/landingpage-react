3a784cae4d6da97799571878c22c0f462f08592c
kwrona05 <kwrona05@gmail.com>
2025-08-16 10:22:10 +0200

    Create footer component with social links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div>
+      <p>Email: kacperwrona.dev@gmail.com</p>
+      <p>LinkedIn: Kacper Wrona</p>
+      <p>Instagram: Kacper Wrona - Aimexa</p>
+    </div>
+  );
+}
