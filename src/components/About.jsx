a940363f7fc21431a2512eb594c9501bead2e2a5
kwrona05 <kwrona05@gmail.com>
2025-08-20 15:40:45 +0200

    ad class names and complete p element

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,19 @@
+import React from "react";
+import "./styles/About.scss";
+
 export default function About() {
   return (
-    <div>
-      <div>
+    <div className="about">
+      <div className="about-content">
         <h2>O mnie</h2>
         <p>
-          Jestem właścicielem firmy Aimexa oraz Konsultantem w firmie
-          Prudential. Moją misją jest pomagać klientom oraz zapewnić im
-          ubezpieczenie dostosowane do ich potrzeb i mozliwości.
+          Jestem właścicielem firmy Aimexa oraz Konsultantem ds. planowania
+          finansowego w <a href="https://www.pru.pl">PRU</a>. Od 2 lat zajmuję
+          się tworzeniem stron internetowych oraz oprogramowania dla firm, szkół
+          oraz klientów prywatnych. Stworzyłem między innymi stronę{" "}
+          <a href="#">zhp.jga</a> oraz aplikację desktopową do układania
+          zastępst za nieobecnych nauczycieli dla szkoły podstawowej nr 5 w
+          Jeleniej Górze.
         </p>
       </div>
     </div>
