7d0daf83c9f72d2b0c676109409a9d8d92e23a43
kwrona05 <kwrona05@gmail.com>
2025-08-20 15:42:08 +0200

    remove useless elements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,27 +2,20 @@ import Header from "./Header";
 import About from "./About";
 import Contact from "./Contact";
 import Offer from "./Offer";
+import Footer from "./Footer";
 
 export default function Home() {
   return (
     <div>
-      <header>
-        <Header />
-      </header>
-      <div>
-        <div>
-          <About />
-        </div>
-        <div>
-          <Offer />
-        </div>
-        <div>
-          <Contact />
-        </div>
-      </div>
-      <footer>
-        <Footer />
-      </footer>
+      <Header />
+
+      <About />
+
+      <Offer />
+
+      <Contact />
+
+      <Footer />
     </div>
   );
 }
