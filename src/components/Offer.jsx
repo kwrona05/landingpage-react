6c90c7dcf76491185cd31e38d1147aed34a0901a
kwrona05 <kwrona05@gmail.com>
2025-08-20 15:42:18 +0200

    add classes

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,22 +1,27 @@
+import React from "react";
+import "./styles/Offer.scss";
+
 export default function Offer() {
   return (
-    <div>
+    <div className="offer">
       <h2>Oferta</h2>
-      <div>
-        <img />
-        <p>Ubezpieczenia</p>
-      </div>
-      <div>
-        <img />
-        <p>Konsultacje i doradztwo</p>
-      </div>
-      <div>
-        <img />
-        <p>Zabezpieczenie Twoich oszczędności</p>
-      </div>
-      <div>
-        <img />
-        <p>Oprogramowanie dostosowane do Twoich potrzeb</p>
+      <div className="offer-grid">
+        <div>
+          <img src="/images/insurance.png" alt="Ubezpieczenia" />
+          <p>Ubezpieczenia</p>
+        </div>
+        <div>
+          <img src="/images/consulting.png" alt="Konsultacje i doradztwo" />
+          <p>Konsultacje i doradztwo</p>
+        </div>
+        <div>
+          <img src="/images/savings.png" alt="Zabezpieczenie oszczędności" />
+          <p>Zabezpieczenie Twoich oszczędności</p>
+        </div>
+        <div>
+          <img src="/images/software.png" alt="Oprogramowanie" />
+          <p>Oprogramowanie dostosowane do Twoich potrzeb</p>
+        </div>
       </div>
     </div>
   );
