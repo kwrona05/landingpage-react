3748770b9b513939e2a9a19416dac06aefb55202
kwrona05 <kwrona05@gmail.com>
2025-08-20 15:41:37 +0200

    complete links and add classes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,33 @@
+import React from "react";
+import "./styles/Footer.scss";
+
 export default function Footer() {
   return (
-    <div>
-      <p>Email: kacperwrona.dev@gmail.com</p>
-      <p>LinkedIn: Kacper Wrona</p>
-      <p>Instagram: Kacper Wrona - Aimexa</p>
+    <div className="footer">
+      <p>
+        Email:{" "}
+        <a href="mailto:kacperwrona.dev@gmail.com">kacperwrona.dev@gmail.com</a>
+      </p>
+      <p>
+        LinkedIn:{" "}
+        <a
+          href="https://www.linkedin.com/in/aimexa-kacper-wrona/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aimexa-Kacper Wrona
+        </a>
+      </p>
+      <p>
+        Instagram:{" "}
+        <a
+          href="https://www.instagram.com/kacperwrona_aimexa/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kacper Wrona - Aimexa
+        </a>
+      </p>
     </div>
   );
 }
