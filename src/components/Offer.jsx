9b041357fc1da457f89a2d56edb5ff47678fc3c6
kwrona05 <kwrona05@gmail.com>
2025-09-14 09:26:01 +0200

    add navigation to offer

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import "./styles/Offer.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Offer() {
+  const navigate = useNavigate();
+
   return (
     <div className="offer">
       <h2>Oferta</h2>
       <div className="offer-grid">
-        <div>
-          <img src="/ubezpieczenie.png" alt="Ubezpieczenia" />
-          <p>Ubezpieczenia na zycie i dozycie</p>
-        </div>
-        <div>
-          <img src="/money.png" alt="Zabezpieczenie oszczędności" />
-          <p>Oszczędności i inwestycje</p>
-        </div>
-        <div>
+        <div onClick={() => navigate("/gadgets")}>
           <img src="/printing.png" alt="Konsultacje i doradztwo" />
-          <p>Technologia NFC i nagrody na eventy</p>
+          <p>Technologia NFC i gadżety na eventy</p>
         </div>
-        <div>
+        <div onClick={() => navigate("/software")}>
           <img src="/websites.png" alt="Oprogramowanie" />
           <p>Oprogramowanie dostosowane do Twoich potrzeb</p>
+        </div>
+        <div onClick={() => navigate("/seo")}>
+          <img src="/SEO.png" alt="SEO" />
+          <p>Optymalizacja SEO dla Twojej strony</p>
         </div>
       </div>
     </div>
