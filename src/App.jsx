bff68f123665421f22ef96cc3e1172fd99e63bcb
kwrona05 <kwrona05@gmail.com>
2025-09-14 09:24:31 +0200

    Create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Software from "./components/Software";
+import Seo from "./components/Seo";
+import Gadgets from "./components/Gadgets";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/software" element={<Software />} />
+        <Route path="/seo" element={<Seo />} />
+        <Route path="/gadgets" element={<Gadgets />} />
       </Routes>
     </Router>
   );
